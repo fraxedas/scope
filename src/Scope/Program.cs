6d7affe5c308932eb9af1df0c15a78bf15b1bb6d
Oscar Fraxedas <--add>
2017-05-04 16:05:14 -0400

    Fix the build

--- src/Scope/Program.cs
+++ src/Scope/Program.cs
@@ -16,7 +16,6 @@ namespace Scope
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
                 .Build();
 
             host.Run();
